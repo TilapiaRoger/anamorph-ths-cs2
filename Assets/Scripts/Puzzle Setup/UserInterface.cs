a2f493d7bb8cc4329546b716c75fa8becf7c2731
TilapiaRoger <jessamyn_lapan@dlsu.edu.ph>
2021-07-19 04:33:30 +0800

    Added an empty new script to set the winning parameters.

--- Assets/Scripts/Puzzle Setup/UserInterface.cs
+++ Assets/Scripts/Puzzle Setup/UserInterface.cs
@@ -5,25 +5,41 @@ using UnityEngine.UI;
 
 public class UserInterface : MonoBehaviour
 {
+    [SerializeField] private Transform modelLocation;
     [SerializeField] private GameObject puzzleModel;
     [SerializeField] private List<GameObject> puzzleModelsList;
     [SerializeField] private Dropdown modelListDropdown;
 
+    private GameObject curModel;
+    private float ticks = 0.0f;
+    private float SPAWN_INTERVAL = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        puzzleModel.SetActive(false);
         initModelList();
-
-        modelListDropdown.onValueChanged.AddListener(delegate
-        {
-            updateModel(modelListDropdown.value);
-        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(curModel != null)
+        {
+            GameObject.Destroy(curModel);
+            curModel = null;
+        }
+        curModel = SpawnDefault(); 
+
+        modelListDropdown.onValueChanged.AddListener(delegate
+        {
+            updateModel(modelListDropdown.value);
+        });
+
+        /*if(ticks > SPAWN_INTERVAL)
+        {
+
+        }*/
     }
 
     void initModelList()
@@ -44,19 +60,35 @@ public class UserInterface : MonoBehaviour
 
     void updateModel(int modelIndex)
     {
-        GameObject.Destroy(puzzleModel);
-        GameObject curModel = Instantiate(puzzleModel);
+        
+        //GameObject.Destroy(puzzleModel);
+        Vector3 position = curModel.transform.localPosition;
+
+        //GameObject.Destroy(puzzleModel);
+        GameObject selectedModel = null;
+
         for (int i = 0; i < puzzleModelsList.Count; i++)
         {
-            if(puzzleModelsList[i] != null && modelIndex == i)
+            if (puzzleModelsList[i] != null && modelIndex == i)
             {
-                curModel = Instantiate(puzzleModelsList[i]);
+                selectedModel = Instantiate(puzzleModelsList[i], modelLocation);
             }
         }
 
-        puzzleModel = curModel;
+        selectedModel.transform.localPosition = position;
+
+        
+        //puzzleModel = curModel;
 
         Debug.Log("Model selected: " + puzzleModel.name + "at index " + modelIndex);
+
     }
 
+    private GameObject SpawnDefault()
+    {
+        GameObject myObj = Instantiate(puzzleModel, modelLocation);
+        myObj.SetActive(true);
+
+        return myObj;
+    }
 }
