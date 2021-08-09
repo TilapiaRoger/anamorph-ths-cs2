f7d89cf80dbbe39e18a8849acc53069002c003cd
TilapiaRoger <jessamyn_lapan@dlsu.edu.ph>
2021-08-09 10:48:42 +0800

    Resolved merge conflicts.

--- Assets/Scripts/Solving/Distribution.cs
+++ Assets/Scripts/Solving/Distribution.cs
@@ -1,67 +1,5 @@
-<<<<<<< HEAD:Assets/Scripts/Solving/Distribution.cs
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using UnityEngine;
 
-public class Distribution : MonoBehaviour
-{
-    public GameObject model;
-    public Transform winningPoint;
-    public Transform modelSpawnPoint;
-    public float minDistance,
-                 oriDistance,
-                 maxDistance = 100F,
-                 scaleFactor;
-    private Vector3 origin = new Vector3(0, 0, 0);
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    Distribution(GameObject model, Transform winningPoint, Transform modelSpawnPoint)
-    {
-        this.model = model;
-        this.winningPoint = winningPoint;
-        this.modelSpawnPoint = modelSpawnPoint;
-    }
-
-    void Distribute()
-    {
-        minDistance = Vector3.Distance(origin, modelSpawnPoint.position);
-        oriDistance = Vector3.Distance(winningPoint.position, modelSpawnPoint.position);
-
-        foreach (Transform child in model.transform)
-        {
-            GameObject piece = child.gameObject;
-
-            // Set the Transform pivot of each slice to the model spawn point by:
-            // Instantiating an empty gameobject.
-            // Moving the empty to the model spawn point.
-            // Parenting the piece to the empty.
-            GameObject pivot = new GameObject();
-            pivot.transform.position = modelSpawnPoint.position;
-            piece.transform.SetParent(pivot.transform);
-
-            // Move piece by a random distance from the model spawn point
-            // between minDistance and maxDistance
-            pivot.transform.position += new Vector3(0, Random.Range(minDistance, maxDistance), 0);
-
-            // Scale the model
-            scaleFactor = minDistance / oriDistance;
-            pivot.transform.localScale *= scaleFactor;
-        }
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
@@ -131,4 +69,3 @@ public class Distribution : MonoBehaviour
         }
     }
 }
->>>>>>> cf3e093f0430489572e24ac5154f026d7c7f91a9:Assets/Scripts/Distribution.cs
