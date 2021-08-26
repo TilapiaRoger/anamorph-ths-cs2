d82565784896878580f9698d5cea0af7bdc93c10
TilapiaRoger <jessamyn_lapan@dlsu.edu.ph>
2021-08-26 21:25:37 +0800

    Refresh commit.

---
+++ Assets/Scripts/Solving/Checkpoints/StartSolving.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSolving : MonoBehaviour
+{
+    [SerializeField] private GameObject puzzleModelLocation;
+    [SerializeField] private PlayerEyeMovement playerAvatar;
+
+    private GameObject puzzleModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartSolve()
+    {
+        Time.timeScale = 1;
+        playerAvatar.StartMove();
+
+        puzzleModel = puzzleModelLocation.transform.GetChild(1).gameObject;
+        Transform[] puzzleModelSlices = puzzleModel.GetComponentsInChildren<Transform>();
+
+        Debug.Log("Model used: " + puzzleModel.name);
+        Debug.Log("Slice count: " + puzzleModel.transform.childCount);
+        Debug.Log("Last child: " + puzzleModel.transform.GetChild(3).gameObject.name);
+        for (int i = 0; i < puzzleModelSlices.Length; i++)
+        {
+            Debug.Log("Slice " + i + ": " + puzzleModelSlices[i].name);
+            puzzleModelSlices[i].gameObject.tag = "Slice";
+            puzzleModelSlices[i].gameObject.AddComponent(typeof(BoxCollider));
+            //puzzleModelSlices[i].gameObject.AddComponent(typeof(Slice));
+        }
+    }
+}
