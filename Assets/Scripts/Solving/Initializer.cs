a4f3e879e039a878b0bbf142bb006c2b81adfab1
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2021-10-22 17:22:45 +0800

    Updated Initializer
    Target's x and y sizes are now scaled to the model's x and y sizes.

--- Assets/Scripts/Solving/Initializer.cs
+++ Assets/Scripts/Solving/Initializer.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Initializer : MonoBehaviour
 {
-    public GameObject modelSpawnPoint,
+    public GameObject model,
+                      modelSpawnPoint,
                       player,
                       winningPoint;
 
@@ -34,9 +35,17 @@ public class Initializer : MonoBehaviour
 
         Debug.Log("d: " + d);
 
+        // Instantiate the model
+        model.SetActive(true);
+        model.transform.localScale = new Vector3(1, 1, 1);
+        model.transform.localEulerAngles = new Vector3(0, 180, 0);
+        Instantiate(model, modelSpawnPoint.transform);
+        model.transform.SetAsFirstSibling();
+
         // Instantiate an invisible cylinder at modelSpawnPoint
         mspDistance = generate(d, gbDistance - d);
         target = GameObject.Find("Target");
+        ResizeTarget(target, model);
         target.transform.SetParent(modelSpawnPoint.transform);
         modelSpawnPoint.transform.position = new Vector3(0f, 0f, mspDistance);
         Debug.Log("Model Spawn Point at " + modelSpawnPoint.transform.position);
@@ -78,5 +87,38 @@ public class Initializer : MonoBehaviour
             d = 30;
             Debug.Log("Contains 08 to 10");
         }
+    }
+    
+    void ResizeTarget(GameObject target, GameObject model)
+    {
+        Bounds modelBounds = GetBounds(model);
+        target.transform.localScale = new Vector3(modelBounds.size.x * model.transform.localScale.x,
+                                                  0,
+                                                  modelBounds.size.z * model.transform.localScale.z);
+    }
+
+    Bounds GetBounds(GameObject model)
+    {
+        Bounds bounds = new Bounds();
+        Renderer[] renderers = model.GetComponentsInChildren<Renderer>();
+
+        if (renderers.Length > 0)
+        {
+            //Find first enabled renderer to start encapsulate from it
+            foreach (Renderer renderer in renderers)
+                if (renderer.enabled)
+                {
+                    bounds = renderer.bounds;
+                    break;
+                }
+
+            //Encapsulate for all renderers
+            foreach (Renderer renderer in renderers)
+                if (renderer.enabled) 
+                    bounds.Encapsulate(renderer.bounds);
+
+        }
+
+        return bounds;
     }
 }
