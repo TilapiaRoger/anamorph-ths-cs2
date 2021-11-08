044a2c262ed627abaa9821a9da41ca84ea05c7b3
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2021-11-08 19:30:57 +0800

    Update Initializer.cs
    Previous issue(s):
    If the player is inside the target's capsule collider, casting rays won't detect target. 
    The winning point is inside target's capsule collider, that's why ray casting doesn't detect anything.
    Solution here:
    Move the target farther from the winning point based on the target's capsule collider bounds

--- Assets/Scripts/Solving/Initializer.cs
+++ Assets/Scripts/Solving/Initializer.cs
@@ -19,7 +19,8 @@ public class Initializer : MonoBehaviour
     // Distances between the origin and specific points
     private float mspDistance,       // model spawn point
                   wpDistance,        // winning point
-                  gbDistance = 100f; // the game bounds
+                  gbDistance = 100f, // the game bounds
+                  greatestBound;     // size of the largest side of the bounding box of the target.
 
     private string modelName;
 
@@ -29,13 +30,15 @@ public class Initializer : MonoBehaviour
         modelParameters = GetComponent<ModelParameters>();
         modelName = modelParameters.modelName;
 
+        modelName = model.name;
+
         SetD(modelName);
 
         Debug.Log("d: " + d);
 
         // Instantiate the model
         model.SetActive(true);
-        model.transform.localScale = new Vector3(1, 1, 1);
+        model.transform.localScale = d / 10 * new Vector3(1, 1, 1);
         model.transform.localEulerAngles = new Vector3(0, 180, 0);
         Instantiate(model, modelSpawnPoint.transform);
         model.transform.SetAsFirstSibling();
@@ -43,8 +46,10 @@ public class Initializer : MonoBehaviour
         // Instantiate an invisible cylinder at modelSpawnPoint
         mspDistance = generate(d, gbDistance - d);
         target = GameObject.Find("Target");
-        ResizeTarget(target, model);
+        greatestBound = ResizeTarget(target, model); // also gets the size of the largest side of the bounding box of the target.
+        Debug.Log(greatestBound);
         target.transform.SetParent(modelSpawnPoint.transform);
+        target.transform.position += new Vector3(0, 0, greatestBound);
         modelSpawnPoint.transform.position = new Vector3(0f, 0f, mspDistance);
         Debug.Log("Model Spawn Point at " + modelSpawnPoint.transform.position);
 
@@ -52,15 +57,6 @@ public class Initializer : MonoBehaviour
         wpDistance = mspDistance - d;
         winningPoint.transform.position = new Vector3(0, 0, wpDistance);
         Debug.Log("Winning Point at " + winningPoint.transform.position);
-
-        // Instantiate an invisible cylinder at modelSpawnPoint
-        target = GameObject.Find("Target");
-        ResizeTarget(target, model);
-        target.transform.SetParent(modelSpawnPoint.transform);
-        modelSpawnPoint.transform.position = new Vector3(0f, 0f, -d);
-        Debug.Log("Model Spawn Point at " + modelSpawnPoint.transform.position);
-
-        
     }
 
     // Update is called once per frame
@@ -80,50 +76,51 @@ public class Initializer : MonoBehaviour
     {
         if (modelName.Contains("01") || modelName.Contains("02") || modelName.Contains("03") || modelName.Contains("04"))
         {
-            d = 5;
+            d = 10;
             Debug.Log("Contains 01 to 04");
         }
             
         else if (modelName.Contains("05") || modelName.Contains("06") || modelName.Contains("07"))
         {
-            d = 10;
+            d = 20;
             Debug.Log("Contains 05 to 07");
         }
         else if (modelName.Contains("08") || modelName.Contains("09") || modelName.Contains("10"))
         {
-            d = 15;
+            d = 30;
             Debug.Log("Contains 08 to 10");
         }
     }
     
-    void ResizeTarget(GameObject target, GameObject model)
+    private float ResizeTarget(GameObject target, GameObject model)
     {
         Bounds modelBounds = GetBounds(model);
         target.transform.localScale = new Vector3(modelBounds.size.x * model.transform.localScale.x,
                                                   0,
                                                   modelBounds.size.z * model.transform.localScale.z);
+        return Mathf.Max(modelBounds.size.x * model.transform.localScale.x,
+                         modelBounds.size.z * model.transform.localScale.z);
     }
 
-    Bounds GetBounds(GameObject model)
+    private Bounds GetBounds(GameObject model)
     {
         Bounds bounds = new Bounds();
         Renderer[] renderers = model.GetComponentsInChildren<Renderer>();
 
-        if (renderers.Length > 0)
+        if(renderers.Length > 0)
         {
             //Find first enabled renderer to start encapsulate from it
-            foreach (Renderer renderer in renderers)
-                if (renderer.enabled)
+            foreach(Renderer renderer in renderers)
+                if(renderer.enabled)
                 {
                     bounds = renderer.bounds;
                     break;
                 }
 
             //Encapsulate for all renderers
-            foreach (Renderer renderer in renderers)
-                if (renderer.enabled) 
+            foreach(Renderer renderer in renderers)
+                if(renderer.enabled) 
                     bounds.Encapsulate(renderer.bounds);
-
         }
 
         return bounds;
