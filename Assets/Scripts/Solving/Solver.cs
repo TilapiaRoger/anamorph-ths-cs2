4d18233bb7455fb11572d9bef0acd9505decc039
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-01-18 23:28:07 +0800

    Add files via upload
    Put an animation for when the player meets the winning conditions

--- Assets/Scripts/Solving/Solver.cs
+++ Assets/Scripts/Solving/Solver.cs
@@ -21,6 +21,9 @@ public class Solver : MonoBehaviour
     private float lookAccuracy,
                   positionAccuracy;
 
+    private bool atWinning,
+                 lookingAtModel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,30 +31,40 @@ public class Solver : MonoBehaviour
 
         mspPosition = modelSpawnPoint.transform.position;
         wpPosition = winningPoint.transform.position;
+
+        Debug.Log("Test: " + Vector3.Distance(mspPosition, wpPosition));
     }
 
     // Update is called once per frame
     void Update()
     {
+        atWinning =  checkPosition();
+        lookingAtModel = checkAngle();
+        
+        // For debug purposes
         results();
+
+        if (atWinning && lookingAtModel && Vector3.Distance(playerPosition, wpPosition) != 0f) animate();
     }
 
     private void results()
     {
-        string positionVerdict = checkPosition() ? "" : "not ",
-               visionVerdict = checkAngle() ? "" : "not ";
+        string positionVerdict = atWinning ? "" : "not ",
+               visionVerdict = lookingAtModel ? "" : "not ";
 
         position.text = "You're " + positionVerdict + "at the winning point\n" +
+                        "Winning Point is at: " + wpPosition + "\n" + 
                         "Position: " + playerPosition;
 
         vision.text = "You're " + visionVerdict + "looking at the target\n" +
+                      "Model Spawn Point is at: " + mspPosition + "\n" +
                       "Angle: " + playerAngle;
     }
 
     private bool checkPosition()
     {
         playerPosition = player.transform.position;
-        return (Vector3.Distance(playerPosition, wpPosition) <= 1) ? true : false;
+        return (Vector3.Distance(playerPosition, wpPosition) <= 1f) ? true : false;
     }
 
     private bool checkAngle()
@@ -65,12 +78,29 @@ public class Solver : MonoBehaviour
         return false;
     }
 
-    // Clamp angles between -180 and 180
+    // Clamp angles displayed between -180 and 180
     private Vector3 clamp(Vector3 angles)
     {
         for(int i = 0; i < 3; i++)
             if(angles[i] > 180) 
                 angles[i] -= 360;
         return angles;
+    }
+
+    void animate()
+    {
+        // Set animation speed
+        float speed = 0.1f,
+              step = speed * Time.deltaTime;
+
+        // move the player
+        player.transform.position = Vector3.MoveTowards(playerPosition, wpPosition, step);
+        playerPosition = player.transform.position;
+
+        // Rotate the player's forward vector towards the model's direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(player.transform.forward, mspPosition - playerPosition, step, 0f);
+
+        // rotate the player towards the model
+        player.transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
