6dbc35ee93ab155bb522d93feea753304c938e68
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-01-29 20:12:24 +0800

    Add files via upload

--- Assets/Scripts/User/PlayerMovement.cs
+++ Assets/Scripts/User/PlayerMovement.cs
@@ -4,38 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 1.0f;
-    public float rotateSpeed = 1.0f;
+    public float movementSpeed = 1.0f,
+                 rotateSpeed = 1.0f;
 
-    public GameObject modelSpawnPoint;
+    public GameObject modelSpawnPoint,
+                      winningPoint;
 
-    private Transform userTransform;
+    private GameObject model,
+                       target,
+                       origin;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    private float yaw = 0.0f,
+                  pitch = 0.0f,
+                  X, Y;
 
     private Vector3 offset,
                     linearVelocity,
-                    angularVelocity;
+                    angularVelocity,
+                    wpPosition,
+                    mspPosition;
 
-    private bool canMove = false;
+    private Transform nearestSlice;
+
+    private bool canMove = false,
+                 shouldSolve = false,
+                 blegh = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.Find("Target");
+        origin = GameObject.Find("Origin");
         StartMove();
-        userTransform = this.gameObject.transform;
         SetPlayerTransform();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canMove == true)
+        if (Input.anyKey) blegh = false;
+
+        if (canMove)
         {
             Pan();
             Rotate();
         }
+
+        // For debugging purposes
+        shouldSolve = Input.GetKey(KeyCode.P);
+        if(shouldSolve && Vector3.Distance(transform.position, wpPosition) != 0) animate();
     }
 
     public void StartMove()
@@ -45,46 +62,58 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetPlayerTransform()
     {
-        yaw = Random.Range(0, 360);
-        pitch = Random.Range(0, 360);
+        // Cache relevant data for optimization purposes.
+        mspPosition = modelSpawnPoint.transform.position;
+        wpPosition = winningPoint.transform.position;
 
-        userTransform.localEulerAngles = new Vector3(pitch, yaw, 0);
+        // set the player’s position 1 unit to the right of the modelSpawnPoint
+        transform.position = mspPosition + modelSpawnPoint.transform.right;
 
-        Vector3 mspPosition = modelSpawnPoint.transform.position;
-
-        float newX = Random.Range(mspPosition.x - 100, mspPosition.x + 100),
-        newY = Random.Range(mspPosition.y - 100, mspPosition.y + 100),
-              newZ = Random.Range(mspPosition.z - 100, mspPosition.z + 100);
-
-        userTransform.position = new Vector3(newX, newY, newZ);
+        // point the camera towards the nearest slice
+        model = GameObject.Find("Model");
+        Transform nearestSlice = findNearest(model.transform);
+        target.transform.position = nearestSlice.position;
+        
+        Debug.Log("Nearest slice: " + nearestSlice.gameObject.name);
+        Debug.Log("Nearest slice Position: " + nearestSlice.position);
+        transform.LookAt(nearestSlice);
     }
 
     private void Pan()
     {
-        linearVelocity = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.Q)) linearVelocity = Vector3.up; 
-        if (Input.GetKey(KeyCode.W)) linearVelocity = Vector3.forward; 
-        if (Input.GetKey(KeyCode.E)) linearVelocity = -Vector3.up; 
-        if (Input.GetKey(KeyCode.A)) linearVelocity = -Vector3.right; 
-        if (Input.GetKey(KeyCode.S)) linearVelocity = -Vector3.forward; 
-        if (Input.GetKey(KeyCode.D)) linearVelocity = Vector3.right;
+        if (Vector3.Distance(gameObject.transform.position, Vector3.zero) < 15)
+        {
+            linearVelocity = new Vector3(0, 0, 0);
+            if (Input.GetKey(KeyCode.Q)) linearVelocity = Vector3.up;
+            if (Input.GetKey(KeyCode.W)) linearVelocity = Vector3.forward;
+            if (Input.GetKey(KeyCode.E)) linearVelocity = -Vector3.up;
+            if (Input.GetKey(KeyCode.A)) linearVelocity = -Vector3.right;
+            if (Input.GetKey(KeyCode.S)) linearVelocity = -Vector3.forward;
+            if (Input.GetKey(KeyCode.D)) linearVelocity = Vector3.right;
 
-        userTransform.Translate(linearVelocity * Time.deltaTime * movementSpeed);
-
-        //Debug.Log("MOVE.");
-
+            transform.Translate(linearVelocity * Time.deltaTime * movementSpeed);
+        }
+        else transform.Translate(-linearVelocity * Time.deltaTime * movementSpeed);
     }
 
     private void Rotate()
     {
+        float xRotation = transform.localEulerAngles.x,
+              yRotation = transform.localEulerAngles.y;
+
 
         if (Input.GetMouseButton(1))
         {
-            yaw += rotateSpeed * Input.GetAxis("Mouse X");
-            pitch -= rotateSpeed * Input.GetAxis("Mouse Y");
-        }
+            yaw   = rotateSpeed * Input.GetAxis("Mouse X");
+            pitch = rotateSpeed * Input.GetAxis("Mouse Y");
+            transform.Rotate(new Vector3(-pitch, yaw, 0));
 
-        userTransform.localEulerAngles = new Vector3(pitch, yaw, 0);
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+
+        }
     }
 
     public void SetMoveSpeed(float moveSpeed)
@@ -95,5 +124,39 @@ public class PlayerMovement : MonoBehaviour
     public void SetRotateSpeed(float rotateSpeed)
     {
         this.rotateSpeed = rotateSpeed;
+    }
+
+    private void animate()
+    {
+        // Set animation speed
+        float speed = 1f,
+              step = speed * Time.deltaTime;
+
+        // move the player
+        transform.position = Vector3.MoveTowards(transform.position, wpPosition, step);
+
+        // rotate the player's forward vector towards the model's direction by one step
+        Vector3 newDirection = (target.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(newDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, step);
+    }
+
+    Transform findNearest(Transform model)
+    {
+        Transform nearest = null;
+        float min = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach(Transform child in model)
+        {
+            Vector3 directionToTarget = child.position - currentPosition;
+            float distanceSquared = directionToTarget.sqrMagnitude;
+            if (distanceSquared < min)
+            {
+                min = distanceSquared;
+                nearest = child;
+            }
+        }
+
+        return nearest;
     }
 }
