067020774b04fd234021ecdf80b517fa805dcf07
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-02-03 16:09:57 +0800

    Update Rotator.cs

--- Assets/Scripts/Solving/Rotator.cs
+++ Assets/Scripts/Solving/Rotator.cs
@@ -27,24 +27,13 @@ public class Rotator : MonoBehaviour
 
     void rotate()
     {
-        float   theta1 = Random.Range(0, 360), // Rotate around the y-axis
-                theta2 = Random.Range(0, 360), // Rotate around the x-axis
-                phi1 = Random.Range(0, 360),   // Rotate around the y-axis
-                phi2,                          // Rotate around the y-axis
-                test;
-
-        do
-        {
-            phi2 = Random.Range(0, 360);
-            test = clamp(theta2 + phi2 - 90);
-        }
-        while (-60 <= test && test <= 60);
-        // Rotate modelSpawnPoint around winningPoint
-        modelSpawnPoint.RotateAround(winningPoint.position, Vector3.up, Random.Range(0, 360));
-        modelSpawnPoint.RotateAround(winningPoint.position, Vector3.right, Random.Range(0, 360));
+        float phi = Random.Range(-180, 180),   // Rotate around the y-axis
+              theta = Random.Range(-60, 60);   // Rotate around the x-axis;
 
         // Rotate modelSpawnPoint and winningPoint around origin
-        winningPoint.RotateAround(origin.position, Vector3.up, Random.Range(0, 360));
-        winningPoint.RotateAround(origin.position, Vector3.right, Random.Range(0, 360));
+        winningPoint.RotateAround(origin.position, Vector3.up, phi);
+
+        // Rotate modelSpawnPoint around winningPoint
+        modelSpawnPoint.RotateAround(winningPoint.position, Vector3.right, theta);
     }
 }
